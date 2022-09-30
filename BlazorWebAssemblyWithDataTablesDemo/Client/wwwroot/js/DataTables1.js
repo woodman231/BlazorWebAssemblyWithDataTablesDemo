a5d329c04f243192b66a23bc555fc0b32fc463b1
Sean Woodward <swoodward@intertech.com>
2022-09-30 09:50:48 -0500

    Add project files.

---
+++ BlazorWebAssemblyWithDataTablesDemo/Client/wwwroot/js/DataTables1.js
@@ -0,0 +1,25 @@
+﻿window.DataTables1 = {
+    dataTable: null,
+    buildDataTable: function () {
+        this.dataTable = $("#dt1").DataTable({
+            "columnDefs": [{
+                "targets": 0,
+                "render": function (data, type, row, meta) {
+                    var dateValue = new Date(data);
+
+                    if (type === 'display') {
+                        return dateValue.toLocaleDateString();
+                    }
+                    else {
+                        return dateValue.valueOf();
+                    }
+                }
+            }],
+        });
+    },
+    destroyDataTable: function () {
+        if (this.dataTable) {
+            this.dataTable.destroy();
+        }        
+    }
+}
---
+++ BlazorWebAssemblyWithDataTablesDemo/Client/wwwroot/js/DataTables2.js
@@ -0,0 +1,39 @@
+﻿window.DataTables2 = {
+    dataTable: null,
+    buildDataTable: function (ajaxUrl) {
+        this.dataTable = $("#dt2").DataTable({
+            "ajax": {
+                "url": ajaxUrl,
+                "dataSrc": "",
+            },
+            "columns": [
+                {
+                    "data": "date",
+                    "render": function (data, type, row, meta) {
+                        var dateValue = new Date(data);
+
+                        if (type === 'display') {                            
+                            return dateValue.toLocaleDateString();
+                        }
+                        else {
+                            return dateValue.valueOf();
+                        }
+                    }
+                },
+                { "data": "temperatureC" },
+                { "data": "temperatureF" },
+                { "data": "summary" },
+            ]
+        });
+    },
+    destroyDataTable: function () {
+        if (this.dataTable) {
+            this.dataTable.destroy();
+        }
+    },
+    refreshDataTable: function () {
+        if (this.dataTable) {
+            this.dataTable.ajax.reload();
+        }
+    }
+}
